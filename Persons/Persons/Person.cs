97e3afb355ed97e63daa9f9ba28294a50569557e
alexashto <alexashto@gmail.com>
2014-03-26 21:08:14 +0400

    MemDB

---
+++ Persons/Persons/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persons
+{
+    class Person
+    {
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+
+        public Person(string fullName, DateTime birthDate)
+        {
+            FullName = fullName;
+            BirthDate = birthDate;
+        }
+    }
+}
